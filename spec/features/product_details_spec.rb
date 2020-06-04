8acd4515f14f12546032a19070547bbc686f05fe
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-06-04 04:54:22 +0000

    added test for clicking and navigating to product detail

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    1.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+  scenario "able to click and visit product page" do 
+    # ACT
+    visit root_path
+    find_link('Details', href: '/products/1').click
+    expect(page).to have_content 'Quantity'
+    save_screenshot
+  end
+end
