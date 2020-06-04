4c2adb9adf5f89d03cf3d0aa78980a19c0b79a74
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-06-04 04:04:05 +0000

    using capybara on home_page

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require "capybara/rails"
-require "capybara/rspec"
-require "capybara/poltergeist" # Add this line to require poltergeist
+
 
 
 ENV['RAILS_ENV'] ||= 'test'
@@ -15,6 +13,9 @@ require 'rspec/rails'
 
 
 # Add additional requires below this line. Rails is not loaded until this point!
+require "capybara/rails"
+require "capybara/rspec"
+require "capybara/poltergeist" # Add this line to require poltergeist
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
